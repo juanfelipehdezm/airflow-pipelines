808680c213758a36a04e01d760381aac60cc57ef
juan felipe hernandez <juanfelipehdezm@example.com>
2022-11-08 17:08:44 -0500

    python operator to download the json file

--- dags/forex_data_pipeline.py
+++ dags/forex_data_pipeline.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 import datetime as dt
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.operators.python import PythonOperator
+import requests
 
 # default arguments for the dag, this are applied to the TASK
 DEFAULT_ARG = {
@@ -13,6 +15,16 @@ DEFAULT_ARG = {
     "retry_delay": dt.timedelta(minutes=5)
 }
 
+
+def download_rates():
+    """function to downloading the json file"""
+
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=COP&apikey=EHCCX9LJ1T4XQV4E'
+    r = requests.get(url)
+    with open("G:/My Drive/Big Data/Airflow-pipelines/files/rates.json", "w") as outfil:
+        outfil.write(r.text)
+
+
 # iniating the dag object
 with DAG("forex_data_pipeline", start_date=dt.datetime(2022, 11, 7),
          schedule_interval="@daily", default_args=DEFAULT_ARG, catchup=False) as dag:
@@ -22,9 +34,14 @@ with DAG("forex_data_pipeline", start_date=dt.datetime(2022, 11, 7),
         # this connection is created on arflow UI
         http_conn_id="forex_api",
         endpoint="/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=COP&apikey=EHCCX9LJ1T4XQV4E",
-        response_check=lambda response: "rates" in response.text,
+        response_check=lambda response: "Rate" in response.text,
         # frecuency in which the sensor check if the condition is match (in these case 5 segs)
         poke_interval=5,
         # after 20 segs or running the record it will stop checking
         timeout=20
+    )
+
+    downloading_rates = PythonOperator(
+        task_id="downloading_rates",
+        python_callable=download_rates
     )
--- test_API.py
+++ test_API.py
@@ -1,7 +1,9 @@
 import requests
+import json
 
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=COP&apikey=EHCCX9LJ1T4XQV4E'
 r = requests.get(url)
-data = r.json()
+#data = json.loads(r.text)
 
-print(data)
+with open("G:/My Drive/Big Data/Airflow-pipelines/files/rates.json", "w") as outfil:
+    outfil.write(r.text)
