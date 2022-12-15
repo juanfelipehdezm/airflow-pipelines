e757954de3f28a3aa3435433bbd9262d37f68fae
juan felipe hernandez <juanfelipehdezm@example.com>
2022-12-14 20:11:05 -0500

    testing dependencies

--- dags/test/test_forexdag_definition.py
+++ dags/test/test_forexdag_definition.py
@@ -36,3 +36,27 @@ class Test_forex_pipeline:
         # print(task_ids)
         assert collections.Counter(task_ids) == collections.Counter(
             self.EXPECTED_TASKS), f"The tasks Ids are different from {self.EXPECTED_TASKS}"
+
+    # declare parameters
+    @pytest.mark.parametrize("task, expected_upstream, expected_downstream",
+
+                             [
+                                 ("is_forex_rates_available",
+                                  [], ["downloading_rates"]),
+                                 ("create_forexRates_database",
+                                  [], ["store_ratings"])
+                             ]
+
+                             )
+    def test_dependencies_of_tasks(self, dag, task, expected_upstream, expected_downstream):
+        """
+        Verify if a given task has the expected upstream and downstream dependencies
+        - Parametrized test function so that each task given in the array is tested with the associated parameters 
+
+        """
+        task = dag.get_task(task, True)
+        print(task)
+        assert collections.Counter(task.upstream_task_ids) == collections.Counter(
+            expected_upstream), f"The task {task} doesn't have the expected upstream dependencies"
+        assert collections.Counter(task.downstream_task_ids) == collections.Counter(
+            expected_downstream), f"The task {task} doesn't have the expected downstream dependencies"
